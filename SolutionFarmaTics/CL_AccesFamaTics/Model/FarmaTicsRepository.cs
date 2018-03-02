58dda423f8f9795a71842f5969eb8525a3d0e5e7
Luisito <metalico_m4@hotmail.com>
2018-03-02 07:32:36 -0600

    Metodos Model Terminado

--- SolutionFarmaTics/CL_AccesFamaTics/Model/FarmaTicsRepository.cs
+++ SolutionFarmaTics/CL_AccesFamaTics/Model/FarmaTicsRepository.cs
@@ -86,6 +86,65 @@ namespace CL_AccesFamaTics.Model
         {
             return db.DetalleVentas;
         }
+
+        public DetalleVenta GetVentaDetailById(int id)
+        {
+            return db.DetalleVentas.Find(id);
+        }
+        public void Add(DetalleVenta np)
+        {
+            db.DetalleVentas.Add(np);
+            Save();
+        }
+        public void Delete(DetalleVenta np)
+        {
+            db.DetalleVentas.Remove(np);
+            Save();
+        }
+        #endregion
+
+        #region Empleado
+        public IQueryable<Empleado> GetallEmployee()
+        {
+            return db.Empleados;
+        }
+
+        public Empleado GetEmployeeById(int id)
+        {
+            return db.Empleados.Find(id);
+        }
+        public void Add(Empleado np)
+        {
+            db.Empleados.Add(np);
+            Save();
+        }
+        public void Delete(Empleado np)
+        {
+            db.Empleados.Remove(np);
+            Save();
+        }
+        #endregion
+
+        #region Categoria
+        public IQueryable<Categoria> GetallCategory()
+        {
+            return db.Categorias;
+        }
+
+        public Categoria GetCategoryById(int id)
+        {
+            return db.Categorias.Find(id);
+        }
+        public void add(Categoria np)
+        {
+            db.Categorias.Add(np);
+            Save();
+        }
+        public void Remove(Categoria np)
+        {
+            db.Categorias.Remove(np);
+            Save();
+        }
         #endregion
 
         public void Save()
